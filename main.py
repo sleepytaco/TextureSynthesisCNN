a65852a63745e3de2f242208078f4a56cd740cd3
Mohammed Khan <abukhan363@gmail.com>
2023-04-29 22:20:26 -0400

    make main runnable

--- main.py
+++ main.py
@@ -2,10 +2,14 @@ from model import TextureSynthesisCNN
 import torch
 import utils
 
+
 def main():
     device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
-    texture_path = ""
+    texture_path = "data/preprocessed_images/banded_0002.jpg"
     texture_exemplar = utils.load_image_tensor(texture_path)  # "path" -> Tensor
     synthesizer = TextureSynthesisCNN(texture_exemplar_image=texture_exemplar, device=device)
     output_image = synthesizer.optimize(num_epochs=10)  # returns Tensor
     utils.save_image_tensor(output_image)  # save Tensor as .jpg/.png file locally
+
+if __name__ == '__main__':
+    main()
